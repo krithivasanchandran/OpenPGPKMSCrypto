9e06704a9bfd9d6474683e1bed43474cc998ec0c
krithivasan <krithivasanchandran@users.noreply.github.com>
2019-03-25 21:10:23 +0530

    Restful Generics

--- src/main/java/com/paypal/service/api/KeymakerAPIImpl.java
+++ src/main/java/com/paypal/service/api/KeymakerAPIImpl.java
@@ -5,25 +5,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+/*
+ * Generic Restful Service Template - Bouncy Castle KMS.
+ * Please extend or edit it to your convenience.
+ */
+
+@Path("/googlekms/v1")
 public class KeymakerAPIImpl implements KeymakerAPI{
 
 	@GET
-	@Path("/world")
+	@Path("/testbed")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayhello(){
-		return "hello welcome Jersey";
+		return "welcome to google KMS";
 	}
 	
 	@Override
-	public String encrypt() {
-		return null;
+	public <T> encrypt<T> {
+	   return method(<T>);
 	}
 
 	@Override
 	public String decrypt() {
-		// TODO Auto-generated method stub
-		return null;
+	   return method(<T>);
 	}
 
 }
